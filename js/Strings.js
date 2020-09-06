ce5b4bde2503d64cc22ff8faded1a52e5add618b
Vlad Bond <foolowin@gmail.com>
2020-09-06 13:54:24 +0300

    tbc

---
+++ js/Strings.js
@@ -0,0 +1,49 @@
+// СТРОКИ
+
+// const name = 'Vlad'
+// const age = 30
+
+// function getAge() {
+//     return age
+// }
+// const output = 'Hello my name ' + name + 'my age ' + age
+// const output = `Hello my name ${name} my age ${getAge()}`
+// const output = `Hello my name ${name} my age ${age < 20 ? 'A' : 'B'}`
+// const output = `
+//     <div>This is div</div>
+//     <p>This is Paragraph</p>
+// `
+// console.log(output)
+
+// const name = 'Vladimir'
+
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.charAt(2))
+// console.log(name.indexOf('!'))
+// console.log(name.startsWith('Vla'))
+// console.log(name.endsWith('ir'))
+// console.log(name.repeat('2'))
+
+// const string = '    password    '
+
+// console.log(string.trimRight())
+// console.log(string.trimStart())
+
+// function logPerson(s, name, age) {
+//     if (age < 0) {
+//         age = 'Dont mess me'
+//     }
+//     return `${s[0]}${name}${s[1]}${age}${s[2]}`
+// }
+// const personName = 'Vladimir'
+// const personName2 = 'Max'
+// const personAge = 30
+// const personAge2 = -30
+
+// const output = logPerson`Name: ${personName}, Age: ${personAge}!`
+// const output2 = logPerson`Name: ${personName2}, Age: ${personAge2}!`
+
+// console.log(output)
+// console.log(output2)
