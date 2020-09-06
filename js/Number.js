c221a108ee0573e7d0942e01fd7a18934230c95b
Vlad Bond <foolowin@gmail.com>
2020-09-06 13:11:31 +0300

    tbc

---
+++ js/Number.js
@@ -0,0 +1,81 @@
+
+// 1 Number
+
+// const num = 42
+// const float = 42.42
+// const pow = 10e3
+
+// console.log(pow)
+
+// КОНСТАНТЫ
+// console.log('MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER)
+// console.log('Math.pow', Math.pow(2, 53) - 1)
+// console.log('MIN_SAFE_INTEGER', Number.MIN_SAFE_INTEGER)
+// console.log('MIN_VALUE', Number.MIN_VALUE)
+// console.log('MAX_VALUE', Number.MAX_VALUE)
+// console.log('POSITIVE_INFINITY', Number.POSITIVE_INFINITY)
+// console.log('NEGATIVE_INFINITY', Number.NEGATIVE_INFINITY)
+// console.log(2 / 0)
+// console.log(NaN) // NOT A NUMBER
+// console.log(typeof NaN)
+// console.log(2 / undefined)
+// const weird = 2 / undefined
+// console.log(isNaN(weird))
+// console.log(isNaN(42))
+// console.log('CHECK INFINITY', Number.isFinite(Infinity))
+// console.log('CHECK 42', Number.isFinite(42))
+// ТАКИЕ ОБЕКТЫ КАК НАМБЕР ЗАРАНИЕ СОЗДАНЫ В ЯЗЫКЕ СО СВОИМИ МЕТОДАМИ
+
+// const stringInt = '40'
+// const stringFloat = '40.42'
+
+// console.log(Number.parseInt(stringInt) + 1)
+// console.log(parseInt(stringInt) + 3)
+// console.log(+stringInt + 2)
+
+// console.log(parseFloat(stringFloat) + 2)
+// console.log(+stringFloat + 2)
+
+// console.log(0.4 + 0.2)
+// console.log((2 / 5) + (1 / 5))
+// console.log(parseFloat((0.4 + 0.2).toFixed(1)))
+
+// 2 BIGINT
+
+// console.log(9007199254740991999999n - 900719925474099199999n)
+// console.log(-9007199254740991999999n)
+// console.log(-9007199254740991999999.321665n) // error
+
+// console.log(10n - 4) // error
+// console.log(parseInt(10n) - 4)
+// console.log(10n - BigInt(4))
+// console.log(5n / 2n) // 2.5n = 2 bigInt все тот же интеджер
+
+// 3 ОБЪЕКТ MATH
+
+// console.log(Math.E)
+// console.log(Math.PI)
+// // квадратный корень
+// console.log(Math.sqrt(25))
+// // возведение в степень
+// console.log(Math.pow(5, 3))
+// // // модуль чисола
+// console.log(Math.abs(-42))
+// console.log(Math.max(442, 12, 30, 5, 11))
+// console.log(Math.min(42, 12, 30, 5, 11))
+// console.log(Math.floor(4.8))
+// console.log(Math.ceil(4.8))
+// console.log(Math.round(4.8))
+// console.log(Math.trunc(4.8))
+// console.log(Math.random(4.8))
+
+// 4   ПРИМЕРЫ
+
+// function getRndBtwn(min, max) {
+//     return Math.random() * (max - min) + min
+// }
+
+// console.log(Math.floor(getRndBtwn(10, 42) + 1))
+
+
+
