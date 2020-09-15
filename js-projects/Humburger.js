8a0898787af9012303daa25a6add9a5d6140fca8
Vlad Bond <foolowin@gmail.com>
2020-09-15 12:19:07 +0300

    done

--- js-projects/Humburger.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector('#btn');
-const nav = document.querySelector('#nav');
-
-// При клике на кнопку добавляем переключатель класса
-btn.addEventListener('click', () => {
-    nav.classList.toggle('active');
-    btn.classList.toggle('active');
-});
